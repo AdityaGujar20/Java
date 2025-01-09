660b65bed5aed29fae6b6f24b65ddd741e237e51
Aditya Gujar <adityagujar20@gmail.com>
2025-01-09 16:20:43 +0530

    Java project added

--- College/Lab-8/src/DatabaseConnection.java
+++ College/Lab-8/src/DatabaseConnection.java
@@ -4,9 +4,15 @@ import java.sql.SQLException;
 
 public class DatabaseConnection {
     public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("org.postgresql.Driver"); // Ensure the driver is loaded
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
         String url = "jdbc:postgresql://localhost:5433/java_lab-8";
-        String user = "postgres";  // Your PostgreSQL username
-        String password = "414618";  // Your PostgreSQL password
+        String user = "postgres";
+        String password = "414618";
         return DriverManager.getConnection(url, user, password);
     }
+    
 }
